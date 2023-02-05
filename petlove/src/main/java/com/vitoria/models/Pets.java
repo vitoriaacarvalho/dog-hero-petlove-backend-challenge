4aca01c70f627312771631519a289fb9e6f572a9
vitoriaacarvalho <vitoriabcarvalhocorrea@gmail.com>
2023-02-05 14:18:19 -0300

    jackson not working

--- petlove/src/main/java/com/vitoria/models/Pets.java
+++ petlove/src/main/java/com/vitoria/models/Pets.java
@@ -1,7 +1,6 @@
 package com.vitoria.models;
 
 import java.util.Date;
-import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,12 +8,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name="pets_table")
@@ -92,11 +88,12 @@ public class Pets {
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
+	
+	@JsonIgnore
 	public DogWalking getWalks() {
 		return walk;
 	}
-
+	@JsonIgnore
 	public void setWalks(DogWalking walks) {
 		this.walk = walk;
 	}
